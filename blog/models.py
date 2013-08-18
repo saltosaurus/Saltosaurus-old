5fe4f79c922a3b9246539fd700e3b3788d98a5a1
salterbw <brian.salter@witty-url.com>
2013-08-17 20:54:47 -0500

    Added blog models.  A blog now has an entry and can store comments on said entry.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
-# Create your models here.
+# A blog entry
+class BlogEntry(models.Model):
+    title = models.CharField(max_length = 100)
+    contents = models.CharField(max_length = 5000)
+    pub_date = models.DateTimeField('Written: ')
+    
+    # Make sure if we view it as an object we get the blog title
+    def __unicode__(self):
+        return self.title
+    
+    # Checks if the entry was written in the last week: returns True if it was
+    def was_written_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=7)
+    
+    
+
+
+# A comment on a blog
+class Comment(models.Model):
+    blogentry = models.ForeignKey(BlogEntry)
+    contents = models.CharField(max_length = 250)
+    author = models.CharField(max_length = 50)
+    
+    # If viewed as an object, show author of comment
+    def __unicode__(self):
+        return self.author
+    
+    
