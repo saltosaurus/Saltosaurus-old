74e07585b865011bbfc409fa05499ebdbb930f0c
salterbw <brian.salter@witty-url.com>
2013-08-20 11:47:06 -0500

    Changed admin URL a bit.

--- WittyURL/urls.py
+++ WittyURL/urls.py
@@ -5,7 +5,7 @@ from WittyURL import views
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^admin', include(admin.site.urls)),
+    url(r'^admin/?', include(admin.site.urls)),
     url(r'^projects', include('projects.urls', namespace="projects")),
     url(r'^blog', include('blog.urls', namespace="blog")),
     # ex: witty-url.com/
