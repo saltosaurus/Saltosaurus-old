7e897372c5986e4ef31a681ee2f12a1316fd16c8
salterbw <brian.salter@witty-url.com>
2013-08-19 21:28:24 -0500

    Fixed issues with forward slashes not being necessary.

--- blog/urls.py
+++ blog/urls.py
@@ -5,9 +5,9 @@ urlpatterns = patterns('',
                        # ex: /blog
                        url(r'^/?$', views.index, name='index'),
                        # ex: /blog/5
-                       url(r'^(?P<blog_id>\d+)/?$', views.entry, name='entry'), 
+                       url(r'^/(?P<blog_id>\d+)/?$', views.entry, name='entry'), 
                        # ex: /blog/5/10
-                       url(r'^(?P<blog_id>\d+)/(?P<comment_id>\d+)/?$', views.comment, name='comment'),
+                       url(r'^/(?P<blog_id>\d+)/(?P<comment_id>\d+)/?$', views.comment, name='comment'),
                        # ex: /blog/5/new_comment/
-                       url(r'^(?P<blog_id>\d+)/new_comment/?$', views.new_comment, name='new_comment'),
+                       url(r'^/(?P<blog_id>\d+)/new_comment/?$', views.new_comment, name='new_comment'),
                        )
