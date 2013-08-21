dc9c851eb551893ef1a3c22aecff77cb7c468025
salterbw <brian.salter@witty-url.com>
2013-08-20 22:41:33 -0500

    Misc no-impact changes.

--- projects/admin.py
+++ projects/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
-from projects.models import Project
-from projects.models import Milestone
+from projects.models import Project, Milestone
 
 class ProjectAdmin(admin.ModelAdmin):
     fieldsets = [
-                 (None,  { 'fields': ('name', 'url', 'description') }), 
-                 ('Timeframe', { 'fields': ('start_date', 'end_date') }),
+                 (None,  { 'fields': ['name', 'url', 'description'] }), 
+                 ('Timeframe', { 'fields': ['start_date', 'end_date'] }),
                 ]
     list_display = ('name', 'url', 'is_completed')
     list_filter = ['start_date', 'end_date', 'is_completed']
