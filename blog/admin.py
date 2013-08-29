855106822b4ac698cc6a68762df16389928626b9
salterbw <brian.salter@witty-url.com>
2013-08-29 17:44:40 -0500

    Removed admin import.

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from blog.models import Article, Comment
-from django.contrib import admin
 from django.contrib.auth.models import Group, User
 from django.contrib.sites.models import Site
 
