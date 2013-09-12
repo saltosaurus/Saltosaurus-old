1e775207005e96419ddf732ffa4d89aaddc7cc21
salterbw <brian.salter@witty-url.com>
2013-09-11 22:51:07 -0500

    Migrated old models for Article/Comment and modified for Flask.

---
+++ app/models.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+from app import db
+
+# An article
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+    contents = db.Column(db.Text)
+    pub_date = db.Column(db.DateTime)
+    comments = db.relationship('Comment', backref = 'article', lazy = 'dynamic')
+    
+    # Make sure if we view it as an object we get the article title
+    def __repr__(self):
+        return self.title
+    
+    # Checks if the article was written in the last week: returns True if it was
+    def was_written_recently(self):
+        return self.pub_date >= datetime.datetime.now() - datetime.timedelta(days=7)
+    
+    
+
+# A comment on an article
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    article_id = db.Column(db.Integer, db.ForeignKey('article.id'))
+    contents = db.Column(db.Text)
+    author = db.Column(db.String(80))
+    pub_date = db.Column(db.DateTime)
+    
+    # If viewed as an object, show author of comment
+    def __repr__(self):
+        return self.author
+    
