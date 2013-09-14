76d6803a5875c0a602899fe541c87810665c31de
salterbw <brian.salter@witty-url.com>
2013-09-14 12:15:50 -0500

    Changed order of articles in Articles page to descending.

--- app/views.py
+++ app/views.py
@@ -7,7 +7,7 @@ from models import Article, Comment
 @app.route('/index')
 @app.route('/index.html')
 def index():
-    latest_article_list = Article.query.order_by(Article.pub_date)[:3]
+    latest_article_list = Article.query.order_by(Article.pub_date.desc())[:3]
     comments_list = Comment.query.order_by(Comment.id) # Linear search is gross
     
     return render_template("index.html", latest_article_list=latest_article_list, comments_list=comments_list)
@@ -15,7 +15,7 @@ def index():
 @app.route('/articles')
 @app.route('/articles.html')
 def articles():
-    latest_article_list = Article.query.order_by(Article.pub_date)[:10]
+    latest_article_list = Article.query.order_by(Article.pub_date.desc())[:10]
     comments_list = Comment.query.order_by(Comment.id)
     
     return render_template('articles.html', latest_article_list=latest_article_list, comments_list=comments_list)
