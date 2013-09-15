8d03a15ee0ddedca52f967023d050e6116bd7de8
salterbw <brian.salter@witty-url.com>
2013-09-14 19:35:10 -0500

    Removed comments_list from index as it is not used.  Also modified other views to use lists instead of query objects.

--- app/views.py
+++ app/views.py
@@ -7,23 +7,22 @@ from models import Article, Comment
 @app.route('/index')
 @app.route('/index.html')
 def index():
-    latest_article_list = Article.query.order_by(Article.pub_date.desc())[:3]
-    comments_list = Comment.query.order_by(Comment.id) # Linear search is gross
+    latest_article_list = Article.query.order_by(Article.pub_date.desc()).all()
     
-    return render_template("index.html", latest_article_list=latest_article_list, comments_list=comments_list)
+    return render_template("index.html", latest_article_list=latest_article_list)
 
 @app.route('/articles')
 @app.route('/articles.html')
 def articles():
-    latest_article_list = Article.query.order_by(Article.pub_date.desc())[:10]
-    comments_list = Comment.query.order_by(Comment.id)
+    latest_article_list = Article.query.order_by(Article.pub_date.desc()).all()
+    comments_list = Comment.query.order_by(Comment.id).all()
     
     return render_template('articles.html', latest_article_list=latest_article_list, comments_list=comments_list)
 
 @app.route('/articles/<int:article_id>')
 def article(article_id):
     article = Article.query.get_or_404(article_id)
-    comments_list = Comment.query.order_by(Comment.id).filter_by(id=article_id)[:5]
+    comments_list = Comment.query.order_by(Comment.id).filter_by(id=article_id).all()
     
     return render_template('article.html', article=article, comments_list=comments_list)
 
@@ -40,3 +39,4 @@ def faq():
 @app.route('/resume')
 def resume():
     return render_template('resume.html')
+
